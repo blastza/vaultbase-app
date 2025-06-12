da55019828066369566349d6793e3a4a37e114e5
Lutendo Damuleli <lutendo.damuleli@outlook.com>
2025-06-12 09:47:18 +0200

    init commit#1

---
+++ src/main/java/com/platform/vaultbase_app/VaultbaseAppApplication.java
@@ -0,0 +1,13 @@
+package com.platform.vaultbase_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaultbaseAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaultbaseAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/platform/vaultbase_app/VaultbaseAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.platform.vaultbase_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaultbaseAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
